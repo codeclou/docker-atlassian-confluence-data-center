c6a72df5e00cbce0229317738e2bd7cf6a5c81e8
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-04-20 17:54:45 +0200

    fix

--- 6.1.2/confluencenode/docker-entrypoint.sh
+++ 6.1.2/confluencenode/docker-entrypoint.sh
@@ -41,7 +41,6 @@ NODE_ID="node${NODE_NUMBER}"
 sed -i -e "s/export CATALINA_OPTS/#replaced/g" /confluence/atlassian-confluence-latest/bin/setenv.sh
 echo -e "CATALINA_OPTS=\"-Dconfluence.cluster.node.name=${NODE_ID} \${CATALINA_OPTS}\"\n" >> /confluence/atlassian-confluence-latest/bin/setenv.sh
 echo -e "CATALINA_OPTS=\"-Dsynchrony.service.url=http://confluence-cluster-612-lb:50612/synchrony/v1 \${CATALINA_OPTS}\"\n" >> /confluence/atlassian-confluence-latest/bin/setenv.sh
-echo -e "CATALINA_OPTS=\"-Djava.awt.headless=true \${CATALINA_OPTS}\"\n" >> /confluence/atlassian-confluence-latest/bin/setenv.sh
 echo -e "\nexport CATALINA_OPTS" >> /confluence/atlassian-confluence-latest/bin/setenv.sh
 
 
