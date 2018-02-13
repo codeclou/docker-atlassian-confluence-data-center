2e9243124e71c58b4f4e61b0f8af48de0ba28764
clouless <github@gruenewaldt.net>
2018-02-13 19:39:56 +0100

    clone

---
+++ PROVISION_CONFLUENCE_DS_VERSIONS.md
@@ -0,0 +1,51 @@
+# How to create new version for new Confluence Version
+
+
+### (0) Install provision script
+
+```
+cp provision-confluence-ds-versions.sh /usr/local/bin/
+chmod +x /usr/local/bin/
+
+mkdir ~/.provision-confluence-ds-versions-workdir/
+```
+
+Note that:
+
+ * script will need you to be authenticated for all GitHub Repos cloned.
+ * docker needs to be installed
+ * script will clone git repos to `~/.provision-confluence-ds-versions-workdir/`
+
+&nbsp;
+
+### (1) Create new Confluence base Image
+
+For every new DataCenter version we first need a new base image in [codeclou/docker-atlassian-base-images](https://github.com/codeclou/docker-atlassian-base-images) repo.
+
+```
+                                     # WHAT              # OLD   # NEW
+provision-confluence-ds-versions.sh  base-image          6.6.0   6.7.1
+```
+
+What the script will do:
+
+ * (1) clone the repo
+ * (2) branch off of oldVersion to newVersion branch
+ * (3) replace version strings in files
+ * (4) trigger a test build of docker image
+
+What you will need to do:
+
+ * (1) check if docker build works
+ * (2) stage, commit and push branch
+ * (3) check docker hub to see if [build for branch succeeds](https://hub.docker.com/r/codeclou/docker-atlassian-base-images/builds/)
+
+
+&nbsp;
+
+### (2) Create new management scripts
+
+```
+                                     # WHAT              # OLD   # NEW
+provision-confluence-ds-versions.sh  management-scripts  6.6.0   6.7.1
+```
---
+++ provision-confluence-ds-versions.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+# USAGE SEE PROVISION_CONFLUENCE_DS_VERSIONS.md
+
+####################################################################################
+# MIT License
+# Copyright (c) 2018 Bernhard Grünewaldt
+# See https://github.com/codeclou/docker-atlassian-confluence-data-center/blob/master/LICENSE
+####################################################################################
+
+set -e
+
+####################################################################################
+#
+# VARS
+#
+####################################################################################
+
+# script params
+ACTION=$1
+LAST_VERSION=$2
+NEW_VERSION=$3
+
+# internal vars
+script_version="2018-02-13 19:19"
+NEW_VERSION_NO_DOTS=${NEW_VERSION//[.]/}
+LAST_VERSION_NO_DOTS=${LAST_VERSION//[.]/}
+
+####################################################################################
+#
+# COLORS
+#
+####################################################################################
+
+export CLICOLOR=1
+C_RED='\x1B[31m'
+C_CYN='\x1B[96m'
+C_GRN='\x1B[32m'
+C_MGN='\x1B[35m'
+C_RST='\x1B[39m'
+
+####################################################################################
+#
+# FUNCTIONS
+#
+####################################################################################
+
+
+# Replace str1 in file by str2
+#
+# @param $1 {string} str1
+# @param $2 {string} str2
+# @param $3 {string} file
+function replace_in_file {
+  str1=$1
+  str2=$2
+  file=$3
+  echo -e $C_GRN"   replace   : ${C_CYN}${str1}${C_RST} by ${C_CYN}${str2}${C_RST} in ${C_CYN}${file}${C_RST}${C_RST}"
+  # Usage with .bak is compatible to macOS and normal linux
+  sed -i .bak "s/${str1}/${str2}/g" ${file}
+  rm -f ${file}.bak
+}
+
+# Rename oldFileName by newFileName
+#
+# @param $1 {string} oldFileName
+# @param $2 {string} newFileName
+function rename_file {
+  oldFileName=$1
+  newFileName=$2
+  echo -e $C_GRN"   renaming  : ${C_CYN}${oldFileName}${C_RST} to ${C_CYN}${newFileName}${C_RST}${C_RST}"
+  mv ${oldFileName} ${newFileName}
+}
+
+# Clone folder dirToClone to dirNameToCloneTo
+#
+# @param $1 {string} dirToClone
+# @param $2 {string} dirNameToCloneTo
+function clone_dir {
+  dirToClone=$1
+  dirNameToCloneTo=$2
+  echo -e $C_GRN"   clone dir : ${C_CYN}${dirToClone}${C_RST} to ${C_CYN}${dirNameToCloneTo}${C_RST}${C_RST}"
+  cp -r ${dirToClone} ${dirNameToCloneTo}
+}
+
+
+# Print action header
+#
+# @param $1 {string} actionname
+function print_action_header {
+  echo -e $C_MGN">>${C_RST}"
+  echo -e $C_MGN">> ACTION: ${1} >  ${LAST_VERSION} -> ${NEW_VERSION} ${C_RST}"
+  echo -e $C_MGN">>${C_RST}"
+  echo ""
+}
+
+#
+# SCRIPT HEADER
+#
+echo ""
+echo -e $C_MGN'°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°º¤ø,¸'$C_RST
+echo ""
+echo -e $C_GRN'  Create new versions of Confluence® Data Center management scripts and docker images'$C_RST
+echo -e $C_MGN'  github  https://git.io/vA3GE'$C_RST
+echo -e $C_CYN"  version $script_version"$C_RST
+echo ""
+echo -e $C_MGN'°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°º¤ø,¸'$C_RST
+echo ""
+
+
+
+
+
+####################################################################################
+#
+# ACTION: management-scripts
+#
+####################################################################################
+if [ "$ACTION" == "management-scripts" ]
+then
+  print_action_header $ACTION
+  echo -e $C_CYN">> trying to clone management scripts on master branch${C_RST}"
+  cd ~/.provision-confluence-ds-versions-workdir/
+  git clone https://github.com/codeclou/docker-atlassian-confluence-data-center.git
+  cd docker-atlassian-confluence-data-center
+  git checkout master > /dev/null 2>&1
+  current_branch=$(git rev-parse --abbrev-ref HEAD)
+  if [ "$current_branch" == "master" ]
+  then
+    echo -e $C_GRN"   we are on master branch ...${C_RST}"
+  else
+    echo -e $C_RED"   we are NOT on master branch. EXIT${C_RST}"
+    exit 1
+  fi
+
+  echo ""
+  echo -e $C_CYN">> trying to create new version ${NEW_VERSION} from old version ${LAST_VERSION}.${C_RST}"
+
+  if [ -d "${NEW_VERSION}" ]
+  then
+    echo -e $C_RED"   new version ${NEW_VERSION} already exists. EXIT${C_RST}"
+    exit 1
+  else
+    echo -e $C_GRN"   new version ${NEW_VERSION} does not yet exist. CONTINUE${C_RST}"
+  fi
+  echo ""
+
+  clone_dir ${LAST_VERSION} ${NEW_VERSION}
+
+  cd ${NEW_VERSION}
+  rename_file     manage-confluence-cluster-${LAST_VERSION}.sh             manage-confluence-cluster-${NEW_VERSION}.sh
+  rename_file     manage-confluence-cluster-${LAST_VERSION}-version.txt    manage-confluence-cluster-${NEW_VERSION}-version.txt
+
+  replace_in_file ${LAST_VERSION}           ${NEW_VERSION}           manage-confluence-cluster-${NEW_VERSION}.sh
+  replace_in_file ${LAST_VERSION}           ${NEW_VERSION}           README.md
+  replace_in_file ${LAST_VERSION_NO_DOTS}   ${NEW_VERSION_NO_DOTS}   manage-confluence-cluster-${NEW_VERSION}.sh
+  replace_in_file ${LAST_VERSION_NO_DOTS}   ${NEW_VERSION_NO_DOTS}   README.md
+
+
+  #git add ./${NEW_VERSION}
+  #git commit -m "automated creation of version ${NEW_VERSION}"
+  #git push
+  #echo -e $C_GRN"   adding new files and pushing to GitHub"
+
+  echo ""
+
+####
+fi
+
+####################################################################################
+#
+# COPY CONFLUENCE-NODE BRANCH
+#
+####################################################################################
+#echo -e $C_CYN">> trying to clone confluencenode-${LAST_VERSION} branch${C_RST}"
+#git checkout confluencenode-${LAST_VERSION} > /dev/null 2>&1
+#current_branch=$(git rev-parse --abbrev-ref HEAD)
+#if [ "$current_branch" == "confluencenode-${LAST_VERSION}" ]
+#then
+#  echo -e $C_GRN"   we are on confluencenode-${LAST_VERSION} branch ...${C_RST}"
+#else
+#  echo -e $C_RED"   we are NOT on confluencenode-${LAST_VERSION} branch. EXIT${C_RST}"
+#  exit 1
+#fi
